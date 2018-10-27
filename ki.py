898a353b5757fa8192a227dc00ad927664076612
Aroniax <arndobber@googlemail.com>
2018-10-27 21:55:45 +0200

    Update ki.py and Add db.sqlite3

--- ki.py
+++ ki.py
@@ -13,40 +13,42 @@ chatbot.set_trainer(ChatterBotCorpusTrainer)
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-string = ''
+voiceInput = ''
+
+def speak(text):
+    os.system('pico2wave -l de-DE -w buffer.wav "' + text + '" && aplay buffer.wav')
 
 while True:
 
-    ja = False
-    while (ja == False):
+    confirmedInput = False
+    while (confirmedInput == False):
 
         try:
             with mic as source:
 
                 r.adjust_for_ambient_noise(source)
                 print('Lausche')
-                os.system('pico2wave -l de-DE -w buffer.wav "Ich höre!" && aplay buffer.wav')
+                speak('Ich höre!')
                 audio = r.listen(source)
 
 
-            string = r.recognize_google(audio, language='de-DE')
-            ja = True
+            voiceInput = r.recognize_google(audio, language='de-DE')
+            confirmedInput = True
         except sr.UnknownValueError:
             print('Error')
-            os.system('pico2wave -l de-DE -w buffer.wav "Das habe ich leider nicht verstanden." && aplay buffer.wav')
+            speak('Das habe ich leider nicht verstanden.')
 
     if bool(re.match('^[ÖÄÜöäüßA-Za-z0-9.,?+=:* ]+$', string)) == True:
 
-        print('Du: ' + string)
+        print('Du: ' + voiceInput)
 
-        answ = str(chatbot.get_response(string))
+        answ = str(chatbot.get_response(voiceInput))
 
         print(str("Kina: ") + answ)
 
         if bool(re.match('^[ÖÄÜöäüßA-Za-z0-9.,?+=:* ]+$', answ)) == False:
             print('WARNUNG')
         else:
-            os.system('pico2wave -l de-DE -w buffer.wav "' +  quote(answ) + '" && aplay buffer.wav')
+            speak(quote(answ))
     else:
         print('WARNUNG: ' + string)
-
